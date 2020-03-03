d0ba815966701f9c2a2d93c4a235d59cb4c4a81d
audeatluci <audeatluci@gmail.com>
2020-03-03 01:23:01 +0000

    Changing recursion in js

--- codeBeers/recursion/javascript/addingNumbers.js
+++ codeBeers/recursion/javascript/addingNumbers.js
@@ -1,3 +1,9 @@
-console.log(
-    [1, 2, 3, 4].reduce((a, b) => a + b, 0)
-  )
+function sum(array, n) {
+  if (n === array.length) {
+      return 0;
+  } else {
+      return array[n] + sum(array, n + 1);
+  }
+}
+
+console.log(sum([1, 2, 3, 4, 5, 6],0))
